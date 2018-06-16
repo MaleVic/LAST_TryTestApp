84fdbc97f50f85c45323b68162330c0a249677bf
Viktor Maleev <gamesmannna@mail.ru>
2018-06-17 00:10:28 +0300

    Реализовано добавление фильма в корзину, а также добавление информации о запросе в базу данных Firebase

--- app/src/main/java/com/example/gamesmannna/trytestapp/Cart.java
+++ app/src/main/java/com/example/gamesmannna/trytestapp/Cart.java
@@ -1,13 +1,21 @@
 package com.example.gamesmannna.trytestapp;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.gamesmannna.trytestapp.Common.Common;
 import com.example.gamesmannna.trytestapp.Database.Database;
 import com.example.gamesmannna.trytestapp.Model.Order;
+import com.example.gamesmannna.trytestapp.Model.Request;
 import com.example.gamesmannna.trytestapp.ViewHolder.CartAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -25,7 +33,7 @@ public class Cart extends AppCompatActivity {
     RecyclerView.LayoutManager layoutManager;
 
     FirebaseDatabase database;
-    DatabaseReference request;
+    DatabaseReference requests;
 
     TextView txtTotalPrice;
     FButton btnPlace;
@@ -41,7 +49,7 @@ public class Cart extends AppCompatActivity {
 
 
         database = FirebaseDatabase.getInstance();
-        request = database.getReference("Requests");
+        requests = database.getReference("Requests");
 
         recyclerView = (RecyclerView) findViewById(R.id.listCart);
         recyclerView.setHasFixedSize(true);
@@ -51,7 +59,56 @@ public class Cart extends AppCompatActivity {
         txtTotalPrice = (TextView) findViewById(R.id.total);
         btnPlace = (FButton) findViewById(R.id.btnPlaceOrder);
 
+        btnPlace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showAlertDialog();
+            }
+        });
+
         loadlistMovie();
+    }
+
+    private void showAlertDialog() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(Cart.this);
+        alertDialog.setTitle("Укажите свои адресные данные");
+        alertDialog.setMessage("Адрес: ");
+
+        final EditText edtAddress = new EditText(Cart.this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+        );
+        edtAddress.setLayoutParams(lp);
+        alertDialog.setView(edtAddress);
+        alertDialog.setIcon(R.drawable.ic_shopping_cart_black_24dp);
+
+        alertDialog.setPositiveButton("ДА", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                Request request = new Request(
+                        Common.currentUser.getPhone(),
+                        Common.currentUser.getName(),
+                        edtAddress.getText().toString(),
+                        txtTotalPrice.getText().toString(),
+                        cart
+                );
+                requests.child(String.valueOf(System.currentTimeMillis()))
+                        .setValue(request);
+                new Database(getBaseContext()).cleanCart();
+                Toast.makeText(Cart.this, "Спасибо. Ваш заказ был размещён", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+
+        alertDialog.setNegativeButton("НЕТ", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        alertDialog.show();
     }
 
     private void loadlistMovie() {
---
+++ app/src/main/java/com/example/gamesmannna/trytestapp/Model/Request.java
@@ -0,0 +1,66 @@
+package com.example.gamesmannna.trytestapp.Model;
+
+import java.util.List;
+
+/**
+ * Created by Gamesmannna on 16.06.2018.
+ */
+
+public class Request {
+private String phone;
+private String name;
+private String address;
+private String total;
+private List<Order> movies;
+
+    public Request() {
+    }
+
+    public Request(String phone, String name, String address, String total, List<Order> movies) {
+        this.phone = phone;
+        this.name = name;
+        this.address = address;
+        this.total = total;
+        this.movies = movies;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public List<Order> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<Order> movies) {
+        this.movies = movies;
+    }
+}
